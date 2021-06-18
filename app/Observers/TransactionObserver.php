0c6567f45b7a98fffab04ae96eff79702588a6b9
Slamet Badwi <slamet.badwi@gmail.com>
2021-06-18 15:24:10 +0700

    Use observer to save user points

---
+++ app/Observers/TransactionObserver.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace App\Observers;
+
+use App\Models\Transaction;
+use App\Models\User;
+
+class TransactionObserver
+{
+    /**
+     * Handle events after all transactions are committed.
+     *
+     * @var bool
+     */
+    public $afterCommit = true;
+
+    /**
+     * Handle the Transaction "created" event.
+     *
+     * @param  \App\Models\Transaction  $transaction
+     * @return void
+     */
+    public function created(Transaction $transaction)
+    {
+        $user = User::find($transaction->user_id);
+        $points = $user->points;
+        $newPoints = (int)$points + (int)$transaction->point->point;
+
+        $user->points = $newPoints;
+        $user->save();
+    }
+
+    /**
+     * Handle the Transaction "updated" event.
+     *
+     * @param  \App\Models\Transaction  $transaction
+     * @return void
+     */
+    public function updated(Transaction $transaction)
+    {
+        //
+    }
+
+    /**
+     * Handle the Transaction "deleted" event.
+     *
+     * @param  \App\Models\Transaction  $transaction
+     * @return void
+     */
+    public function deleted(Transaction $transaction)
+    {
+        //
+    }
+
+    /**
+     * Handle the Transaction "restored" event.
+     *
+     * @param  \App\Models\Transaction  $transaction
+     * @return void
+     */
+    public function restored(Transaction $transaction)
+    {
+        //
+    }
+
+    /**
+     * Handle the Transaction "force deleted" event.
+     *
+     * @param  \App\Models\Transaction  $transaction
+     * @return void
+     */
+    public function forceDeleted(Transaction $transaction)
+    {
+        //
+    }
+}
