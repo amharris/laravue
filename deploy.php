0e863d73bc2ba76b528ec5186ebe6f01426409ec
Slamet Badwi <slamet.badwi@gmail.com>
2021-06-22 11:36:03 +0700

    User Deployer for github actions

---
+++ deploy.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace Deployer;
+
+require 'recipe/laravel.php';
+require 'contrib/rsync.php';
+
+set('application', 'Laravel App');
+set('ssh_multiplexing', true);
+
+set('rsync_src', function () {
+    return __DIR__;
+});
+
+
+add('rsync', [
+    'exclude' => [
+        '.git',
+        '/.env',
+        '/storage/',
+        '/vendor/',
+        '/node_modules/',
+        '.github',
+        'deploy.php',
+    ],
+]);
+
+task('deploy:secrets', function () {
+    file_put_contents(__DIR__ . '/.env', getenv('DOT_ENV'));
+    upload('.env', get('deploy_path') . '/shared');
+});
+
+host('production')
+  ->setHostname(getenv('SSH_HOST'))
+  ->setPort(getenv('SSH_PORT'))
+  ->setTag('production')
+  ->setRemoteUser(getenv('SSH_USER'))
+  ->setDeployPath(getenv('DEPLOY_PATH'));
+
+after('deploy:failed', 'deploy:unlock');
+
+desc('Deploy the application');
+
+task('deploy', [
+    'deploy:info',
+    'deploy:prepare',
+    'deploy:lock',
+    'deploy:release',
+    'rsync',
+    'deploy:secrets',
+    'deploy:shared',
+    'deploy:vendors',
+    'deploy:writable',
+    'artisan:storage:link',
+    'artisan:view:cache',
+    'artisan:config:cache',
+    'artisan:migrate',
+    'artisan:queue:restart',
+    'deploy:symlink',
+    'deploy:unlock',
+    'cleanup',
+]);
